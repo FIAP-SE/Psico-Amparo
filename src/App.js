53fe7776088e1393886fe3a14126a9c4f34559ff
guswinn <gustavohenrique.jesus@outlook.com>
2025-06-05 21:56:22 +1000

    add pag PsicologoAssociado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './pages/Home/Home';
 import Agendamento from './pages/Agendamento/Agendamento';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PsicologoAssociado from './pages/PsicologoAssociado/PsicologoAssociado';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agendamento" element={<Agendamento />} />
+        <Route path="/PsicologoAssociado" element={<PsicologoAssociado />} />  
+        
       </Routes>
     </Router>
   );
---
+++ src/pages/PsicologoAssociado/PsicologoAssociado.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './style.css';
+import Header from '../../components/Header/Header';
+
+
+const PsicologoAssociado = () => {
+  return (  
+    <div> 
+        
+        <Header />
+         
+        <div className="perfil-container">
+        <div className="perfil-sidebar">
+            <div className="foto-placeholder"></div>
+            <p><strong>Nome:</strong> Alexandre Floriani Goulart</p>
+            <p><strong>Email:</strong> alexdregoulart@gmail.com</p>
+            <p><strong>Telefone:</strong> (11) 99815-5632</p>
+            <p><strong>CPF:</strong> 296.581.652-36</p>
+            <p><strong>Estado:</strong> São Paulo</p>
+            <p><strong>Cidade:</strong> São Paulo</p>
+            <button className="botao-editar">ALTERAR DADOS</button>
+        </div>
+
+        <div className="perfil-agendamentos">
+            <div className="agendamento-section">
+            <h4>PRÓXIMO HORÁRIO</h4>
+            <div className="agendamento-card">
+                <div className="foto-circulo"></div>
+                <div className="agendamento-info">
+                <p><strong>RAFAEL SIQUEIRA VOSS</strong></p>
+                <p>Psicólogo CRP 05/26598</p>
+                <p>Dia 15/06 às 11:30</p>
+                </div>
+                <button className="botao-sala">Entrar na sala virtual</button>
+            </div>
+            </div>
+
+            <div className="agendamento-section">
+            <h4>HISTÓRICO</h4>
+
+            <div className="agendamento-card">
+                <div className="foto-circulo"></div>
+                <div className="agendamento-info">
+                <p><strong>RAFAEL SIQUEIRA VOSS</strong></p>
+                <p>Psicólogo CRP 05/26598</p>
+                <p>Dia 10/05 às 14:00</p>
+                </div>
+            </div>
+
+            <div className="agendamento-card">
+                <div className="foto-circulo"></div>
+                <div className="agendamento-info">
+                <p><strong>ALESSANDRA MARINHO ALVES</strong></p>
+                <p>Psicólogo CRP 02/33856</p>
+                <p>Dia 31/04 às 8:50</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default PsicologoAssociado;
